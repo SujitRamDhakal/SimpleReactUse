de44401e762ac63ee640de42d25627ba89286a21
sujit <sujitramdhakal59@gmail.com>
2023-10-11 20:28:44 +0545

    learned how to use useState hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+import "./App.css";
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [step, setStep] = useState(1);
+  const handleNext = () => {
+    if (step < 3) setStep(step + 1);
+  };
+  const handlePrevious = () => {
+    if (step > 1) setStep(step - 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isOpen ? (
+        <>
+          <Numbers step={step} />
+          <Main step={step} />
+          <Control step={step} nex={handleNext} prev={handlePrevious} />
+          <button onClick={() => setIsOpen(!isOpen)}>Close</button>
+        </>
+      ) : (
+        <>
+          <button onClick={() => setIsOpen(!isOpen)}>Open</button>
+        </>
+      )}
     </div>
   );
-}
-
+};
 export default App;
+
+const Numbers = ({ step }) => {
+  return (
+    <div className="numbers">
+      <li className={step >= 1 ? "num" : ""}>1</li>
+      <li className={step >= 2 ? "num" : ""}>2</li>
+      <li className={step >= 3 ? "num" : ""}>3</li>
+    </div>
+  );
+};
+const Main = ({ step }) => {
+  const message = ["learn react", "then practice", "finally earn"];
+  return (
+    <div className="main">
+      <h1 className="main">{message[step - 1]}</h1>
+    </div>
+  );
+};
+const Control = ({ prev, nex }) => {
+  return (
+    <div className="control">
+      <button className="button" onClick={() => prev()}>
+        Previous
+      </button>
+      <button className="button" onClick={() => nex()}>
+        Next
+      </button>
+    </div>
+  );
+};
